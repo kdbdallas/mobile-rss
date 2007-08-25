b41cc9c68be0f64298f59c3eeac51a9e1b0871f2
kdbdallas <kdbdallas@cb179fbe-3038-0410-87e3-5b3a9cbdf46e>
2007-08-25 06:51:28 +0000

    Tons of updates! First release! v0.5

--- trunk/source/ItemView.m
+++ trunk/source/ItemView.m
@@ -2,30 +2,39 @@
 #import "ItemView.h"
 
 @implementation ItemView
-- (id)initWithFrame:(CGRect)frame atIndex:(int)row withFeedItemTitles:(NSArray *)feedItemTitles withFeedItemDesc:(NSArray *)feedItemDesc
+- (id)initWithFrame:(CGRect)frame
 {
-	//itemView = [[UIView alloc] initWithFrame: frame];
-	
-	self = [super initWithFrame:frame];
-	
+//	[super initWithFrame: frame];
+
 	navBar = [[UINavigationBar alloc] initWithFrame: CGRectMake(0.0f, 0.0f, 320.0f, 40.0f)];
 	[navBar showButtonsWithLeftTitle: @"Back" rightTitle: @"Home" leftBack: TRUE];
     [navBar setBarStyle: 3];
 	[navBar setDelegate: self];
 	[self addSubview: navBar];
-	//[navBar setPrompt: [@"Digg: " stringByAppendingString:[[feedItemTitles objectAtIndex:row] retain]]];
 
 	textView = [[UITextView alloc] initWithFrame: CGRectMake(0.0f, 40.0f, 320.0f, frame.size.height - 40.0f)];
     [textView setEditable:NO];
     [textView setTextSize:14];
-	[textView setText: [[feedItemDesc objectAtIndex:row] retain]];
 
 	[self addSubview: textView];
 }
 
-/*- (void)dealloc {
-	[ navBar release ];
-	[ super dealloc ];
-}*/
+- (void)fillWithData:(NSArray *)feedItemTitles withFeedItemDesc:(NSArray *)feedItemDesc
+{
+	//_feedItemTitles = feedItemTitles;
+	//_feedItemDesc = feedItemDesc;
+}
+
+- (void)showDataAtIndex:(int)row
+{
+	//[navBar setPrompt: [@"Digg: " stringByAppendingString:[[feedItemTitles objectAtIndex:row] retain]]];
+	//	[textView setText: [[feedItemDesc objectAtIndex:row] retain]];
+}
+
+- (void)dealloc {
+//	[ navBar release ];
+//	[ textView release ];
+//	[ super dealloc ];
+}
 
 @end
--- trunk/source/MobileRSS.h
+++ trunk/source/MobileRSS.h
@@ -13,13 +13,23 @@
 #import <UIKit/UIAlertSheet.h>
 #import <UIKit/UIProgressHUD.h>
 #import <UIKit/UITransitionView.h>
-#import "ItemView.h"
+#import <UIKit/UITextView.h>
+#import <UIKit/UINavigationBar.h>
+#import <UIKit/UIPreferencesTable.h>
+#import <UIKit/UIPreferencesTableCell.h>
+#import <UIKit/UIPreferencesTextTableCell.h>
+#import <UIKit/UISwitchControl.h>
+#import <UIKit/UIPushButton.h>
+#import <WebCore/WebFontCache.h>
+#import <UIKit/UINavBarButton.h>
 
 @interface MobileRSS : UIApplication {
         UIWindow *window;
 		UIView *mainView;
+		UIView *itemView;
+		UIView *settingsView;
 		UINavigationBar *navBar;
-		NSString *URL;
+		UITextLabel *title;
 		NSMutableData *_responseData;
 		UITable* _viewTable;
 		UITableColumn* _viewTableCol;
@@ -31,11 +41,26 @@
 		NSArray *alertButton;
 		UIProgressHUD *progress;
 		UITransitionView *transitionView;
+		UITextView *textView;
 		NSMutableArray *feedItemTitles;
 		NSMutableArray *feedItemDesc;
+		NSMutableArray *feedItemLinks;
+		NSMutableArray *feedItemDates;
+		NSString *_fromView;
+		NSURLConnection *theConnection;
+		int _clickedRow;
+		UITransitionView *_transitionView;
+		NSString *_settingsPath;
+		UIPreferencesTable *_prefsTable;
+		UIPreferencesTableCell *_prefsHeader;
+		UIPreferencesTextTableCell *_prefsFeed;
+		NSString *feedTitle;
 }
 
 - (void) handleError: (NSError*) err;
+- (void)getFeed:(NSString *)URL;
+- (NSString*)loadSettings;
+- (void)setTitle;
 - (void) parseXMLDocument:(NSXMLDocument *)document;
 - (void) connectionDidFinishLoading:(NSURLConnection *)connection;
 - (void)reloadData;
@@ -43,6 +68,13 @@
 - (int)numberOfRowsInTable:(UITable *)table;
 - (UITableCell *)table:(UITable *)table cellForRow:(int)row column:(UITableColumn *)col;
 - (void)showProgressHUD:(NSString *)label withWindow:(UIWindow *)w withView:(UIView *)v withRect:(struct CGRect)rect;
+- (void)navigationBar:(UINavigationBar*)navbar buttonClicked:(int)button;
+- (void) showSettings;
+- (void) readSettings;
+- (void) writeSettings;
+- (void) closeSettings;
+- (void) saveAndCloseSettings;
+- (void) visitLink;
 - (void)hideProgressHUD;
 
 @end
